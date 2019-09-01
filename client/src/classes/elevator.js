6e304ef0f7ebde2eb99484969dcf37a6263376be
Alex <alexkoshernosiegov@gmail.com>
2019-09-01 19:30:37 +0300

    Added elevator logic with sorting remaining floors

--- client/src/classes/elevator.js
+++ client/src/classes/elevator.js
@@ -1,5 +1,5 @@
-import {elevatorStates} from './elevator_states_enum'
-import {isBetween} from './utils'
+import { elevatorStates } from './elevator_states_enum'
+import { isBetween, partialSort, ascendingOrderComparator, descendingOrderComparator } from './utils'
 
 class Elevator {
   constructor () {
@@ -18,34 +18,37 @@ class Elevator {
     this.state = state
   }
 
-  async removeCurrentFloor () { // is ought to be called then elevator arrives at floor
-    await this.floors[0].shift()
+  removeCurrentFloor () { // is ought to be called then elevator arrives at floor
+    this.floors[0].shift()
   }
 
-  async addFloor (floor, direction) {
+  addFloor (floor, direction) {
     if (this.floor.includes(floor)) { // it's pointless to add floor if queue contains one
       return
     }
     if (this.state === elevatorStates.still) { // if elevator queue is empty
-      await this.floors.push(floor) // insert target to queue
+      this.floors.push(floor) // insert target to queue
     }
     if (this.state === direction === elevatorStates.goingUp) { // if elevetor's going up and person wants to go up
       if (isBetween(this.floors[0], this.floors[1], floor)) { // and if new floor is between current and target (e.x from 1 to 5 and button pressed at 3 ^)
-        await this.floors.splice(1, 0, floor) // pushing new flor bettwen current and target marking new floor as new target
+        this.floors.splice(1, 0, floor) // pushing new flor bettwen current and target marking new floor as new target
       }
       if ((floor > this.floors[this.floors.length - 1]) || (floor < this.floors[0])) { // if new floor as higher than last in queue or lower than first
-        await this.floors.push(floor) // push it to the top (making last in queue)
+        this.floors.push(floor) // push it to the top (making last in queue)
       }
+      partialSort(this.floors, ascendingOrderComparator, 2) // sort all floors except for current and target in ascending order
+      return
     }
     if (this.state === direction === elevatorStates.goingDown) {
       if (isBetween(this.floors[0], this.floors[1], floor)) {
-        await this.floors.splice(1, 0, floor) //  similar to previous in goingUp: pushing level in between two neighboors
+        this.floors.splice(1, 0, floor) //  similar to previous in goingUp: pushing level in between two neighboors
       }
       if (floor < this.floors[this.floors.length - 1]) {
-        await this.floors.push(floor)
+        this.floors.push(floor)
       }
+      partialSort(this.floors, descendingOrderComparator) // sort all floors except for current and target in descending order
     } else {
-      await this.floors.push(floor)
+      this.floors.push(floor)
     }
   }
 }
